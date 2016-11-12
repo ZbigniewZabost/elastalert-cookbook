69738cded5d1cef20d4ffe7a1c98c1917b833dbf
zbigniew <zabostz@gmail.com>
2016-11-12 20:12:29 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# elastalert
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: elastalert
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
