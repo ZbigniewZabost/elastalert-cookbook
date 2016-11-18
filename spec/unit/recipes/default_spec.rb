2ffbfb76bffad14ee168f1735651a8c2569a46d4
zbigniew <zabostz@gmail.com>
2016-11-18 20:36:19 +0100

    add unit tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,9 +1,3 @@
-#
-# Cookbook Name:: elastalert
-# Spec:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
-
 require 'spec_helper'
 
 describe 'elastalert::default' do
@@ -13,12 +7,60 @@ describe 'elastalert::default' do
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+    describe 'when elastalert is being installed' do
+      context 'for the 1st time' do
+        before do
+          allow(::File).to receive(:exist?).with(anything).and_call_original
+          allow(::File).to receive(:exist?).with('/opt/elastalert/.env').and_return(false)
+          allow(::File).to receive(:exist?).with('/opt/elastalert/.env/bin/elastalert-create-index').and_return(false)
+          stub_command("curl -XGET 'http://localhost:9200/elastalert_status/' -s | grep '@timestamp'").and_return(false)
+        end
 
-    it 'clones elastalert repo' do
-      expect(chef_run).to checkout_git('elastalert').with(revision: 'v0.1.3')
+        it 'converges successfully' do
+          expect { chef_run }.to_not raise_error
+        end
+
+        it 'installs elastalert' do
+          expect(chef_run).to run_python_execute('/opt/elastalert/setup.py install')
+        end
+
+        it 'installs pip requriments' do
+          resource = chef_run.python_execute('/opt/elastalert/setup.py install')
+          expect(resource).to notify('pip_requirements[/opt/elastalert/requirements.txt]').to(:install).immediately
+        end
+
+        it 'creates elastalert ES index' do
+          expect(chef_run).to run_python_execute('setup elastalert index')
+        end
+      end
+
+      context 'for the next time' do
+        before do
+          allow(::File).to receive(:exist?).with(anything).and_call_original
+          allow(::File).to receive(:exist?).with('/opt/elastalert/.env').and_return(true)
+          allow(::File).to receive(:exist?).with('/opt/elastalert/.env/bin/elastalert-create-index').and_return(true)
+          stub_command("curl -XGET 'http://localhost:9200/elastalert_status/' -s | grep '@timestamp'").and_return(true)
+        end
+
+        it 'converges successfully' do
+          expect { chef_run }.to_not raise_error
+        end
+
+        it 'does not reinstall elastalert' do
+          resource = chef_run.python_execute('/opt/elastalert/setup.py install')
+          expect(resource).to do_nothing
+        end
+
+        it 'does not reinstall pip requriments' do
+          resource = chef_run.pip_requirements('/opt/elastalert/requirements.txt')
+          expect(resource).to do_nothing
+        end
+
+        it 'does not tries to create ES index' do
+          resource = chef_run.python_execute('setup elastalert index')
+          expect(resource).to do_nothing
+        end
+      end
     end
   end
 end
