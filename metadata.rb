2cd92786fdf3f0c3fe6cba6592fd07232af66316
zbigniew <zabostz@gmail.com>
2016-11-20 18:33:10 +0100

    remove debian version constraint

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures elastalert'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-supports 'debian', '= 8'
+supports 'debian'
 
 depends 'poise-python'
 depends 'git'
